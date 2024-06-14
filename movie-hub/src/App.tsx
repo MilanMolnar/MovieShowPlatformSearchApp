ca750743ddcdda2abe3ed9458000b0efad6ccf7c
Molnar Milan <molnarmilan.cc@gmail.com>
2024-06-14 16:24:37 +0200

    fixies tailwind config and creates a basic flexible layout

--- movie-hub/src/App.tsx
+++ movie-hub/src/App.tsx
@@ -1,9 +1,15 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
 import "./App.css";
 
 function App() {
-  return <>initial</>;
+  return (
+    <div className="flex flex-col h-screen">
+      <header className="border p-4">Navigation Bar</header>
+      <div className="flex-grow flex overflow-auto">
+        <aside className="border hidden sm:block w-64 p-4">Sidebar</aside>
+        <main className="border flex-grow p-4">Main Content</main>
+      </div>
+    </div>
+  );
 }
 
 export default App;
