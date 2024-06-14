e89f7da62989902fd274a64eca3d066bbe2c8468
Molnar Milan <molnarmilan.cc@gmail.com>
2024-06-14 18:08:29 +0200

    builds a basic implementation of darkmode switch with tailwind

---
+++ movie-hub/src/components/ColorModeSwitch.tsx
@@ -0,0 +1,48 @@
+import React, { useState, useEffect } from "react";
+
+const DarkModeToggle = () => {
+  const [darkMode, setDarkMode] = useState(() => {
+    const saved = localStorage.getItem("darkMode");
+    return saved !== null ? JSON.parse(saved) : true; // default to dark mode
+  });
+
+  useEffect(() => {
+    localStorage.setItem("darkMode", JSON.stringify(darkMode));
+    if (darkMode) {
+      document.documentElement.classList.add("dark");
+    } else {
+      document.documentElement.classList.remove("dark");
+    }
+  }, [darkMode]);
+
+  const toggleDarkMode = () => {
+    setDarkMode(!darkMode);
+  };
+
+  return (
+    <div className="relative inline-block w-10 align-middle select-none transition duration-200 ease-in">
+      <input
+        type="checkbox"
+        name="toggle"
+        id="toggle"
+        checked={darkMode}
+        onChange={toggleDarkMode}
+        className="toggle-checkbox absolute block w-6 h-6 rounded-full bg-white appearance-none cursor-pointer opacity-0"
+      />
+      <label
+        htmlFor="toggle"
+        className={`toggle-label block overflow-hidden h-6 rounded-full ${
+          darkMode ? "bg-gray-300" : "bg-green-500"
+        } cursor-pointer`}
+      >
+        <span
+          className={`toggle-dot absolute w-6 h-6 rounded-full shadow inset-y-0 left-0 transform transition-transform duration-200 ease-in-out ${
+            darkMode ? "translate-x-4 bg-black" : "translate-x-0 bg-white"
+          }`}
+        ></span>
+      </label>
+    </div>
+  );
+};
+
+export default DarkModeToggle;
--- movie-hub/src/components/NavBar.tsx
+++ movie-hub/src/components/NavBar.tsx
@@ -1,14 +1,21 @@
 import React from "react";
 import logo from "../assets/logo.webp";
+import ColorModeSwitch from "./ColorModeSwitch";
 
 const NavBar = () => {
   return (
     //want to create a horizontally stacking items for navbar component, the fist element should be the logo.webp found in the public folder use tailwind where aplicable
     <nav className="flex items-center p-2">
       <img src={logo} alt="logo" className="w-16 rounded shadow" />
-      <ul className="flex">
-        <li className="ml-4">Profile</li>
-      </ul>
+      <div className="flex justify-between w-full">
+        <ul id="left-nav" className="flex">
+          <li className="ml-4">search</li>
+        </ul>
+        <ul id="right-nav" className="flex">
+          <ColorModeSwitch />
+          <li className="ml-4">Profile</li>
+        </ul>
+      </div>
     </nav>
   );
 };
