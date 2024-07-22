99872efbe929e034c4d72618ee53b540a41bea0f
Molnar Milan <molnarmilan.cc@gmail.com>
2024-07-22 20:12:27 +0200

    fixes discover filter when only a region is selected

--- movie-hub/src/hooks/useTvShows.ts
+++ movie-hub/src/hooks/useTvShows.ts
@@ -16,17 +16,23 @@ export interface TvShow {
 
 const useTvShows = (
   genres: Genre[],
-  selecterRegion: Region,
+  selectedRegion: Region,
   selectedPlatform: Platform | null
 ) => {
   const genreIds = genres.map((genre) => genre.id).join(",");
 
-  if (!selectedPlatform) {
+  if (!genres.length) {
     return useData<TvShow>(
-      `/3/discover/tv?with_genres=${genreIds}&watch_region=HU`
+      `/3/discover/tv?include_adult=true&language=en-US&watch_region=${selectedRegion?.iso_3166_1}&with_watch_monetization_types=flatrate`
     );
   }
-  if (!selecterRegion) {
+
+  if (!selectedPlatform) {
+    return useData<TvShow>(
+      `/3/discover/tv?with_genres=${genreIds}&watch_region=${selectedRegion?.iso_3166_1}`
+    );
+  }
+  if (!selectedRegion) {
     return useData<TvShow>(
       `/3/discover/tv?with_genres=${genreIds}&with_watch_providers=${selectedPlatform?.provider_id}&watch_region=HU`
     );
@@ -34,7 +40,7 @@ const useTvShows = (
 
   return useData<TvShow>(
     //generate an endpoint with the selected genres and platform for the movie database api
-    `/3/discover/tv?include_adult=true&language=en-US&page=1&sort_by=popularity.desc&watch_region=${selecterRegion.iso_3166_1}&with_genres=${genreIds}&with_watch_monetization_types=flatrate%7Cbuy%7Cfree%7Crent%7Cads&with_watch_providers=${selectedPlatform?.provider_id}`
+    `/3/discover/tv?include_adult=true&language=en-US&page=1&sort_by=popularity.desc&watch_region=${selectedRegion.iso_3166_1}&with_genres=${genreIds}&with_watch_monetization_types=flatrate%7Cbuy%7Cfree%7Crent%7Cads&with_watch_providers=${selectedPlatform?.provider_id}`
   );
 };
 
