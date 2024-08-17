8d5975db010aeeb407651cb78dbff32d895bfa43
Molnar Milan <molnarmilan.cc@gmail.com>
2024-08-17 17:27:13 +0200

    Implements auth badge on profile page

--- movie-hub/src/pages/ProfilePage.tsx
+++ movie-hub/src/pages/ProfilePage.tsx
@@ -1,9 +1,27 @@
+// ProfilePage.tsx
+import React from "react";
+import { useAuth } from "../providers/AuthContextProvider";
 import AuthContainer from "../components/AuthContainer";
+import BadgeScene from "../components/BadgeScene";
 
 const ProfilePage = () => {
+  const { userProfile } = useAuth();
+
   return (
-    <div className="p-10">
-      <AuthContainer></AuthContainer>
+    <div className="w-full flex">
+      <div className="p-4 pl-20 pt-36 ">
+        <AuthContainer />
+      </div>
+      {/* Conditionally render BadgeScene only if user is logged in */}
+      {userProfile && (
+        <div className="flex-1">
+          <BadgeScene
+            userEmail={userProfile.email}
+            userName={userProfile.name}
+            userPicture={userProfile.picture}
+          />
+        </div>
+      )}
     </div>
   );
 };
